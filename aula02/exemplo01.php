e35b0a9d60ec0490e2c6467445b0ea2238bed12f
marcossbalbino <161628490+marcossbalbino@users.noreply.github.com>
2025-02-13 18:24:11 -0300

    primeira aula

---
+++ aula02/exemplo01.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Oi 2F, sou o Andersson</p>
+
+    <?php
+      $nome = "Andersson";
+      echo "<p>Oi 2F, sou o Andersson<p>";
+      echo "<p>Oi 2F, sou o $nome </p>";
+      echo "<p>Oi 2F, sou o ".$nome."</p>";
+    ?>
+
+
+</body>
+</html>
---
+++ aula02/exemplo02.php
@@ -0,0 +1,7 @@
+<?php
+    $a=4;
+    $b="dalmatas 101";
+    $s = $a + $b;
+
+    echo $s;
+?>    
