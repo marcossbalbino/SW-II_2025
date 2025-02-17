23899d6ee669bb100371d3a3196cb925ba2064db
marcossbalbino <161628490+marcossbalbino@users.noreply.github.com>
2025-02-17 15:00:17 -0300

    Add files via upload

---
+++ exercicios01.php
@@ -0,0 +1,13 @@
+<?php
+    $nome = "Marcos";
+    $idade = "16";
+    $valor = "13";
+
+    echo "Olá, Qual o seu nome?";
+    echo $nome;
+    echo "Oi, $nome! Qual a sua idade?";
+    echo $idade;
+    echo "Então você tem $idade!!!";
+    echo "Quanto você ganha? $nome?";
+    echo "$valor";
+    echo "$valor! Eu espero que seja por hora e não por ano!!!!! rsrsrsrs...";
